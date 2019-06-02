b368ec4c3611a8f3a915c614c376afd17d92929b
wanguohui <wanguohui@yahoo.com>
2019-06-02 14:38:03 +0800

    two sum array

---
+++ php/167-two-sum.php
@@ -0,0 +1,58 @@
+<?php
+class Solution 
+{
+    /**
+     * @param Integer[] $numbers
+     * @param Integer $target
+     * @return Integer[]
+     */
+	//利用有序性，利用对撞指针
+    public function twoSum($numbers, $target) 
+	{
+        $count = count($numbers);
+        $i = 0;
+        $j = $count - 1;
+        while ($i < $j) {
+            $value = $numbers[$i] + $numbers[$j];
+            if ($value == $target) {
+                return [$i+1, $j+1];
+            } else if ($value < $target) {
+                $i++;
+            } else {
+                $j--;
+            }
+        }
+    }
+	//用二分查找
+	public function twoSumV2($numbers, $target)
+	{
+		$count = count($numbers);
+		for ($i = 0; $i < $count; $i++) {
+			$need = $target - $numbers[$i];
+			if (($j = $this->binarySearch($numbers, $need, $i+1, $count-1)) > -1) {
+				return [$i+1, $j+1]; 
+			}
+		}
+	}
+	private function binarySearch($numbers, $target, $start, $end)
+	{
+		while ($start <= $end) {
+			$middle = intval(($end-$start)/2) + $start;
+			if ($numbers[$middle] == $target) {
+				return $middle;
+			} else if ($numbers[$middle] > $target) {
+				$end = $middle-1;
+			} else {
+				$start = $middle+1;
+			}
+		}
+		return -1;
+	}
+}
+
+//test
+$numbers = [2,3,4];
+$target = 6;
+$s = new Solution();
+
+var_dump($s->twoSumV2($numbers, $target));
