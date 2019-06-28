98903f4144ce7b7cb81e03763b42d2c39b67b34c
wanguohui <wanguohui@yahoo.com>
2019-06-28 16:49:20 +0800

    52 n queens 2

---
+++ php/52-n-queens2.php
@@ -0,0 +1,46 @@
+<?php
+class Solution {
+    private $res = [];
+    private $col;
+    private $diagonal1;
+    private $diagonal2;
+    /**
+     * @param Integer $n
+     * @return Integer
+     */
+    function totalNQueens($n) {
+        // x column
+        $this->col = array_fill(0, $n, false);
+        //lower left to upper right
+        $this->diagonal1 = array_fill(0, 2*n-1, false);
+        //upper left to lower right
+        $this->diagonal2 = $this->diagonal1;
+        
+        $this->putQueen($n, 0, []);
+        
+        return count($this->res);
+    }
+
+    function putQueen($n, $index, $row) {
+        if ($index == $n) {
+            $this->res[] = $row;
+            return;
+        }
+        for ($j = 0; $j < $n; $j++) {
+            if (!$this->col[$j] && !$this->diagonal1[$index+$j] && !$this->diagonal2[$index-$j+$n-1]) {
+                $row[$index] = $j;
+                $this->col[$j] = true;
+                $this->diagonal1[$index+$j] = true;
+                $this->diagonal2[$index-$j+$n-1] = true;
+                
+                $this->putQueen($n, $index+1, $row);
+                
+                $this->col[$j] = false;
+                $this->diagonal1[$index+$j] = false;
+                $this->diagonal2[$index-$j+$n-1] = false;
+                unset($row[$index]);
+            }
+        }
+        return;
+    }
+}
