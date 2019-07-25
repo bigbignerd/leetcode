81ec1efe778fd7cd50d568506cabdb7a9f7c300f
wanguohui <wanguohui@yahoo.com>
2019-07-25 17:05:08 +0800

    102 binary tree level order traversal

---
+++ go/102-binary-tree-level-order-traversal.go
@@ -0,0 +1,44 @@
+package main
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+type Node struct {
+	tn    *TreeNode
+	level int
+}
+
+func levelOrder(root *TreeNode) [][]int {
+	if root == nil {
+		return [][]int{}
+	}
+	var resMap map[int][]int = make(map[int][]int)
+	var queue []Node
+	queue = append(queue, Node{root, 0})
+
+	for len(queue) > 0 {
+		n := queue[0]
+		tn := n.tn
+		level := n.level
+		queue = queue[1:]
+		resMap[level] = append(resMap[level], tn.Val)
+		if tn.Left != nil {
+			queue = append(queue, Node{tn.Left, level + 1})
+		}
+		if tn.Right != nil {
+			queue = append(queue, Node{tn.Right, level + 1})
+		}
+	}
+	var res [][]int
+
+	for i := 0; i < len(resMap); i++ {
+		res = append(res, resMap[i])
+	}
+
+	return res
+}
